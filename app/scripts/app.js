ec0ce5e10e5d8b7694b6bba523fbe5b521db80ae
chapulina <burajiru.no.chapulina@gmail.com>
2016-09-01 15:08:52 -0700

    tests

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,12 +13,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     console.log('WebComponentsReady')
-
-
-
-
-
-
   });
 
 })(document);
