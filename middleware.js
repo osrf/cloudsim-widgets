b8de67242876c083295a6745c92d6216f9d3045e
chapulina <burajiru.no.chapulina@gmail.com>
2016-09-01 14:48:19 -0700

    add hardcoded users

--- middleware.js
+++ middleware.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 
 dotenv.config()
 
+// Required environment variables
 if (!process.env.CLOUDSIM_AUTH_URL)
   throw('undefined: process.env.CLOUDSIM_AUTH_URL')
 if (!process.env.CLOUDSIM_PORTAL_URL)
@@ -12,6 +13,8 @@ if (!process.env.AUTH0_CLIENT_ID)
   throw('undefined: process.env.AUTH0_CLIENT_ID')
 if (!process.env.AUTH0_DOMAIN)
   throw('undefined: process.env.AUTH0_DOMAIN')
+if (!process.env.CLOUDSIM_ADMIN)
+  throw('undefined: process.env.CLOUDSIM_ADMIN')
 
 exports.middleware = function(req, res, next){
   if (req.originalUrl === "/scripts/config.js") {
@@ -25,7 +28,12 @@ exports.middleware = function(req, res, next){
            portal: "${process.env.CLOUDSIM_PORTAL_URL}",
            sim: "${process.env.CLOUDSIM_SIM_URL}",
            auth0_id: "${process.env.AUTH0_CLIENT_ID}",
-           auth0_domain: "${process.env.AUTH0_DOMAIN}"
+           auth0_domain: "${process.env.AUTH0_DOMAIN}",
+           admin: "${process.env.CLOUDSIM_ADMIN}",
+           src_competitor: "${process.env.SRC_COMPETITOR}",
+           src_admin: "${process.env.SRC_ADMIN}",
+           ariac_competitor: "${process.env.ARIAC_COMPETITOR}",
+           ariac_admin: "${process.env.ARIAC_ADMIN}",
          }
       }
     `
