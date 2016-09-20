5b73ac9ebe94e0529ef8d42e50a1550a4a32ed35
Hugo Boyer <hugo@osrfoundation.org>
2016-09-20 10:56:27 -0700

    PR comments: missing cloudsim-grant, package.json improvements

--- server/cloudsim_widgets.js
+++ server/cloudsim_widgets.js
@@ -3,7 +3,6 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-const pack = require('../package')
 const bodyParser = require("body-parser")
 const cors = require('cors')
 const morgan = require('morgan')
