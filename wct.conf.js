955cb844a4c3a8d4a8b6a2d4214ef574d06dad5e
chapulina <burajiru.no.chapulina@gmail.com>
2016-10-25 14:48:17 -0700

    coverage for front end

--- wct.conf.js
+++ wct.conf.js
@@ -1,18 +1,33 @@
 var path = require('path');
 
-var ret = {
-  'suites': ['app/test'],
-  'webserver': {
-    'pathMappings': []
-  }
-};
-
 var mapping = {};
 var rootPath = (__dirname).split(path.sep).slice(-1)[0];
 
 mapping['/components/' + rootPath  +
 '/app/bower_components'] = 'bower_components';
 
-ret.webserver.pathMappings.push(mapping);
+var ret = {
+  'verbose' : false,
+  'suites': ['app/test'],
+  'webserver': {
+    'pathMappings': [mapping]
+  },
+  'plugins': {
+    'istanbul': {
+      'dir': './coverage',
+      'reporters': ['text-summary', 'lcov'],
+      'include': [
+        '/app/elements/**/*.html'
+      ],
+      "exclude": [
+      ],
+      'thresholds': {
+        'global': {
+          'statements': 10
+        }
+      }
+    }
+  }
+};
 
 module.exports = ret;
